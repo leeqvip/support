58ee42b78f7df5632b6b60fe0d646ac356d45112
TechLee <techlee@qq.com>
2018-09-17 16:51:12 +0800

    init

---
+++ src/Dumper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TechOne\Support;
+
+/**
+ *
+ */
+class Dumper
+{
+    public static function dump(...$vars)
+    {
+        ob_start();
+
+        call_user_func_array('var_dump', $vars);
+
+        $output = ob_get_clean();
+        $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+
+        if (PHP_SAPI == 'cli') {
+            $output = PHP_EOL . $output . PHP_EOL;
+        } else {
+            $output = '<pre>' . $output . '</pre>';
+        }
+        exit($output);
+    }
+}
